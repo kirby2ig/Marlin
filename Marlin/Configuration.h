3e5099ce8d1f6d24b00a980654f00eba7dcd3839
Ben Marshall <kirby27@verizon.net>
2020-02-21 23:46:13 -0500

    Set ABS Bed Preheating

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1468,7 +1468,7 @@
 
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED      0
+#define PREHEAT_2_TEMP_BED      100
 #define PREHEAT_2_FAN_SPEED   0 // Value from 0 to 255
 
 /**
